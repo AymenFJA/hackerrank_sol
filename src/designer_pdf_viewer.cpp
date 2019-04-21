5e15e5b8a0bd5e0dfa0d3f5aff17a75277013340
Aymen Alsaadi <27039262+AymenFJA@users.noreply.github.com>
2019-04-21 13:23:50 -0400

    Update designer_pdf_viewer.cpp

--- src/designer_pdf_viewer.cpp
+++ src/designer_pdf_viewer.cpp
@@ -1,31 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-	vector<int> h1 = { 1 ,3 ,1, 3 ,1 ,4 ,1 ,3 ,2 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 };
-	string word1="abc";
-	vector<int> h = { 1 ,3 ,1 ,3 ,1 ,4 ,1 ,3 ,2 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,7 };
-	string word = "zaba";
-	char c;
-	int pos = 0;
-	int ascii=0;
-	int length = 0;
-	
-	vector<int> poss;
-	int max = 0;
-	for (int i = 0; i < word.length(); i++) 
-	{	
-	
-		c = word[i];
-		ascii = int(c);
-		pos = ascii - 97;
-		poss.insert(poss.begin(),(h[pos]));
-	}
-	
-	max = *max_element(poss.begin(), poss.end());
-	length = max * word.length();
+vector<string> split_string(string);
 
-	return length;
+// Complete the designerPdfViewer function below.
+int designerPdfViewer(vector<int> h, string word) {
+  char c;
+  int pos = 0;
+  int ascii = 0;
+  int length = 0;
+
+  vector<int> poss;
+  int max = 0;
+  for (int i = 0; i < word.length(); i++) {
+
+    c = word[i];
+    ascii = int(c);
+    pos = ascii - 97;
+    poss.insert(poss.begin(), (h[pos]));
+  }
+
+  max = *max_element(poss.begin(), poss.end());
+  length = max * word.length();
+
+  return length;
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
 }

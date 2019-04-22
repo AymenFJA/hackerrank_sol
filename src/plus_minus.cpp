9f960c5a2e3673c87f27b9c6f18ae5727e9c5d43
Aymen Alsaadi <27039262+AymenFJA@users.noreply.github.com>
2019-04-21 23:10:48 -0400

    Create plus_minus.cpp

---
+++ src/plus_minus.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+  int neg = 0;
+  int pos = 0;
+  int zer = 0;
+  float div1, div2, div3;
+  int n = arr.size();
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < 0)
+      neg++;
+    else if (arr[i] > 0)
+      pos++;
+    else
+      zer++;
+  }
+  div1 = (float)pos / (float)n;
+  div2 = (float)neg / (float)n;
+  div3 = (float)zer / (float)n;
+
+  cout <<div1<<endl;
+  cout << div2<<endl;
+  cout <<div3<<endl;
+
+  system("pause");
+    }
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
